538b5931389c1484b74556ccd9f85369863bac41
rakib3719 <rakib3719>
2026-02-27 14:19:53 +0600

    update notificaiton settings

--- src/notification/dto/create-notification.dto.ts
+++ src/notification/dto/create-notification.dto.ts
@@ -1,1 +1,22 @@
-export class CreateNotificationDto {}
+import { IsMongoId, IsOptional, IsString } from 'class-validator';
+import { UserIdDto } from 'src/common/dto/mongoId.dto';
+import { NotificationType } from 'src/schemas/notification.schema';
+
+export class CreateNotificationDto {
+  @IsMongoId({ message: 'Receiver is Required' })
+  receiver: UserIdDto['userId'];
+
+  @IsOptional()
+  @IsMongoId()
+  sender: UserIdDto['userId'];
+
+  @IsString({ message: 'Message is Required' })
+  message: string;
+
+  @IsString({ message: 'Notification Type is Required' })
+  type: NotificationType;
+
+  @IsOptional()
+  @IsString()
+  redirectLinik: string;
+}
--- src/schemas/notification.schema.ts
+++ src/schemas/notification.schema.ts
@@ -1,32 +1,31 @@
 import { Prop, Schema } from '@nestjs/mongoose';
-import { IsNotEmpty, IsOptional } from 'class-validator';
-import { Types } from 'mongoose';
+import { UserIdDto } from 'src/common/dto/mongoId.dto';
 
 export enum NotificationType {
-  //
-  ALERT = 'ALERT',
-  REMINDER = 'REMINDER',
-  ACTIVITY = 'ACTIVITY',
-  LIVE = 'LIVE',
-  MARKET = 'MARKET',
-  DOCUMENT_REMINDERS = 'DOCUMENT_REMINDERS',
-  USER_REPORT = 'USER_REPORT',
-  MODERATION = 'MODERATION',
+  ALERT = 'ALERT', //*When a property matching their saved search criteria is listed.
+  REMINDER = 'REMINDER', //*For upcoming open houses they registered for.
+  ACTIVITY = 'ACTIVITY', //*When a favorites property is sold, rented, or desisted.
+  LIVE = 'LIVE', // *When their property is published on the site/portal.
+  MARKET = 'MARKET', //*Weekly/monthly digest emails with new listings and market trends.
+  DOCUMENT_REMINDERS = 'DOCUMENT_REMINDERS', //*For lease agreements, inspection reports, etc.
+  USER_REPORT = 'USER_REPORT', //*New user registrations, flagged accounts, suspicious activity.
+  MODERATION = 'MODERATION', //*New listings pending approval, flagged listings.
 }
 
 @Schema({ timestamps: true })
 export class Notification {
-  @Prop({ required: true })
-  receiver: Types.ObjectId;
+  @Prop({ required: true, ref: 'User' })
+  receiver: string;
 
-  @IsOptional()
-  @Prop({ required: false })
-  sender: Types.ObjectId;
+  @Prop({ required: false, ref: 'User' })
+  sender: string;
 
-  @IsNotEmpty()
   @Prop({ required: true })
   message: string;
 
   @Prop({ enum: NotificationType })
   type: NotificationType;
+
+  @Prop({ required: false })
+  redirectLink: string;
 }

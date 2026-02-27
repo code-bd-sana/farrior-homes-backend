c3f1222357566d9c4817f72153bb00aeb28f3275
Ishrat2413 <ishratrintu2413@gmail.com>
2026-02-27 10:21:22 +0600

    feat: update user schema and module for Mongoose integration

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,10 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 
-export type AuthDocument = HydratedDocument<Auth>;
+export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true, versionKey: false })
-export class Auth {
+export class User {
   @Prop({ trim: true })
   // can upload image as file or provide image url
   profileImage?: string;
@@ -43,4 +43,4 @@ export class Auth {
   linkedinLink?: string;
 }
 
-export const AuthSchema = SchemaFactory.createForClass(Auth);
+export const UserSchema = SchemaFactory.createForClass(User);

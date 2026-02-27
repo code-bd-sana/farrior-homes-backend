0c9737a7c85aa9b49928b10e8864e5b184866ad4
rakib3719 <rakib3719>
2026-02-27 09:41:28 +0600

    response interceptor configure

--- src/common/interceptor/response-interceptor/response-interceptor.interceptor.ts
+++ src/common/interceptor/response-interceptor/response-interceptor.interceptor.ts
@@ -1,9 +1,33 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
 
 @Injectable()
 export class ResponseInterceptorInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+    const ctx = context.switchToHttp();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const response = ctx.getResponse();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const request = ctx.getRequest();
+
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        message: 'Request successful',
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        method: request.method,
+        endpoint: request.url,
+        statusCode: response.statusCode,
+        timestamp: new Date().toISOString(),
+        data: data,
+      })),
+    );
   }
 }

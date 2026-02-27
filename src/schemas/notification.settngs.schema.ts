1ff60f441adbf4385c2ffd8ed8ab1ca61a576c65
rakib3719 <rakib3719>
2026-02-27 15:10:51 +0600

    update notificaiton settings schema

--- src/schemas/notification.settngs.schema.ts
+++ src/schemas/notification.settngs.schema.ts
@@ -1,13 +1,19 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { NotificationType } from './notification.schema';
 
+export enum NotificationSettingsTitle {
+  ALERT = 'New Listing Alerts',
+}
 @Schema({ timestamps: true })
 export class NotificationSettings {
   @Prop({ required: true, unique: true })
   name: NotificationType;
-
+  @Prop({ required: true, unique: true })
+  title: string;
   @Prop({ required: true })
   isActive: boolean;
+  @Prop({ required: true })
+  description: string;
 }
 export const NotificationSettingSchema =
   SchemaFactory.createForClass(NotificationSettings);

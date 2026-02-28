11f1bf7ecc0c494148054ecf24ae1e53b9306e50
rakib3719 <bannah76769@gmail.com>
2026-02-28 13:44:32 +0600

    Teast auto commit

--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -7,6 +7,8 @@ import { CreatePropertyDto } from './dto/create-property.dto';
 import { UpdatePropertyDto } from './dto/update-property.dto';
 import { Property } from 'src/schemas/property.schema';
 
+
+// doc
 @Injectable()
 export class PropertyService {
   constructor(
